b21f3c854700727e0091c633bb15d81b754080fa
TranNgocChi <brightsuntnc2003@gmail.com>
2022-11-13 01:27:59 +0700

    first commit

---
+++ core/models.py
@@ -0,0 +1,52 @@
+from venv import create
+from django.db import models
+from django.contrib.auth import get_user_model
+import uuid
+from datetime import datetime
+
+User = get_user_model()
+
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True)
+    profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
+    location = models.CharField(max_length=100, blank=True)
+
+
+    def __str__(self):
+        return self.user.username
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='post_images',  default='blank-profile-picture.png')
+    caption = models.TextField()
+    create_at = models.DateTimeField(default=datetime.now())
+    no_of_likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user
+
+class LikePost(models.Model):
+    post_id = models.CharField(max_length=500)
+    username = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
+
+class FollowersCount(models.Model):
+    follower = models.CharField(max_length=100)
+    user = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user
+
+class Comment(models.Model):
+    comment = models.CharField(max_length=1000)
+    user = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.comment
